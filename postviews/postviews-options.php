b071d558da034473953dbf6d762b3e347102ce71
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-30 08:17:28 +0000

    1.20
    git-svn-id: https://plugins.svn.wordpress.org/wp-postviews/tags/1.20@20429 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ postviews/postviews-options.php
@@ -0,0 +1,206 @@
+<?php
+/*
++----------------------------------------------------------------+
+|																							|
+|	WordPress 2.1 Plugin: WP-PostViews 1.20									|
+|	Copyright (c) 2007 Lester "GaMerZ" Chan									|
+|																							|
+|	File Written By:																	|
+|	- Lester "GaMerZ" Chan															|
+|	- http://lesterchan.net															|
+|																							|
+|	File Information:																	|
+|	- Post Views Options Page														|
+|	- wp-content/plugins/postviews/postviews-options.php				|
+|																							|
++----------------------------------------------------------------+
+*/
+
+
+### Variables Variables Variables
+$base_name = plugin_basename('postviews/postviews-options.php');
+$base_page = 'admin.php?page='.$base_name;
+$id = intval($_GET['id']);
+$mode = trim($_GET['mode']);
+$views_settings = array('views_options', 'widget_views_most_viewed');
+$views_postmetas = array('views');
+
+
+### Form Processing 
+if(!empty($_POST['do'])) {
+	// Decide What To Do
+	switch($_POST['do']) {
+		case __('Update Options', 'wp-postviews'):
+			$views_options = array();
+			$views_options['count'] = intval($_POST['views_count']);
+			$views_options['template'] =  addslashes(trim($_POST['views_template_template']));
+			$update_views_queries = array();
+			$update_views_text = array();
+			$update_views_queries[] = update_option('views_options', $views_options);
+			$update_views_text[] = __('Post Views Options', 'wp-postviews');
+			$i=0;
+			$text = '';
+			foreach($update_views_queries as $update_views_query) {
+				if($update_views_query) {
+					$text .= '<font color="green">'.$update_views_text[$i].' '.__('Updated', 'wp-postviews').'</font><br />';
+				}
+				$i++;
+			}
+			if(empty($text)) {
+				$text = '<font color="red">'.__('No Post Views Option Updated', 'wp-postviews').'</font>';
+			}
+			break;
+		//  Uninstall WP-PostViews
+		case __('UNINSTALL WP-PostViews', 'wp-postviews') :
+			if(trim($_POST['uninstall_views_yes']) == 'yes') {
+				echo '<div id="message" class="updated fade">';
+				echo '<p>';
+				foreach($views_settings as $setting) {
+					$delete_setting = delete_option($setting);
+					if($delete_setting) {
+						echo '<font color="green">';
+						printf(__('Setting Key \'%s\' has been deleted.', 'wp-postviews'), "<strong><em>{$setting}</em></strong>");
+						echo '</font><br />';
+					} else {
+						echo '<font color="red">';
+						printf(__('Error deleting Setting Key \'%s\'.', 'wp-postviews'), "<strong><em>{$setting}</em></strong>");
+						echo '</font><br />';
+					}
+				}
+				echo '</p>';
+				echo '<p>';
+				foreach($views_postmetas as $postmeta) {
+					$remove_postmeta = $wpdb->query("DELETE FROM $wpdb->postmeta WHERE meta_key = '$postmeta'");
+					if($remove_postmeta) {
+						echo '<font color="green">';
+						printf(__('Post Meta Key \'%s\' has been deleted.', 'wp-postviews'), "<strong><em>{$postmeta}</em></strong>");
+						echo '</font><br />';
+					} else {
+						echo '<font color="red">';
+						printf(__('Error deleting Post Meta Key \'%s\'.', 'wp-postviews'), "<strong><em>{$postmeta}</em></strong>");
+						echo '</font><br />';
+					}
+				}
+				echo '</p>';
+				echo '</div>'; 
+				$mode = 'end-UNINSTALL';
+			}
+			break;
+	}
+}
+
+
+### Determines Which Mode It Is
+switch($mode) {
+		//  Deactivating WP-PostViews
+		case 'end-UNINSTALL':
+			$deactivate_url = 'plugins.php?action=deactivate&amp;plugin=postviews/postviews.php';
+			if(function_exists('wp_nonce_url')) { 
+				$deactivate_url = wp_nonce_url($deactivate_url, 'deactivate-plugin_postviews/postviews.php');
+			}
+			echo '<div class="wrap">';
+			echo '<h2>'.__('Uninstall WP-PostViews', 'wp-postviews').'</h2>';
+			echo '<p><strong>'.sprintf(__('<a href="%s">Click Here</a> To Finish The Uninstallation And WP-PostViews Will Be Deactivated Automatically.', 'wp-postviews'), $deactivate_url).'</strong></p>';
+			echo '</div>';
+			break;
+	// Main Page
+	default:
+		$views_options = get_option('views_options');
+?>
+<script type="text/javascript">
+	/* <![CDATA[*/
+	function views_default_templates(template) {
+		var default_template;
+		switch(template) {
+			case 'template':
+				default_template = "<?php _e('%VIEW_COUNT% views', 'wp-postviews'); ?>";
+				break;
+		}
+		document.getElementById("views_template_" + template).value = default_template;
+	}
+	/* ]]> */
+</script>
+<?php if(!empty($text)) { echo '<!-- Last Action --><div id="message" class="updated fade"><p>'.$text.'</p></div>'; } ?>
+<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>"> 
+<div class="wrap"> 
+	<h2><?php _e('Post Views Options', 'wp-postviews'); ?></h2> 
+	<fieldset class="options">
+		<legend><?php _e('Post Views Options', 'wp-postviews'); ?></legend>
+		<table width="100%"  border="0" cellspacing="3" cellpadding="3">
+			 <tr valign="top">
+				<th align="left" width="30%"><?php _e('Count Views From:', 'wp-postviews'); ?></th>
+				<td align="left">
+					<select name="views_count" size="1">
+						<option value="0"<?php selected('0', $views_options['count']); ?>><?php _e('Everyone', 'wp-postviews'); ?></option>
+						<option value="1"<?php selected('1', $views_options['count']); ?>><?php _e('Guests Only', 'wp-postviews'); ?></option>
+						<option value="2"<?php selected('2', $views_options['count']); ?>><?php _e('Registered Users Only', 'wp-postviews'); ?></option>
+					</select>
+				</td>
+			</tr>
+			<tr valign="top">
+				<th align="left" width="30%"><?php _e('Views Template:', 'wp-postviews'); ?></th>
+				<td align="left">
+					<input type="text" id="views_template_template" name="views_template_template" size="70" value="<?php echo htmlspecialchars(stripslashes($views_options['template'])); ?>" /><br />
+						<?php _e('HTML is allowed.', 'wp-postviews'); ?><br />
+						%VIEW_COUNT% - <?php _e('The number of views.', 'wp-postviews'); ?><br />
+						<input type="button" name="RestoreDefault" value="<?php _e('Restore Default Template', 'wp-postviews'); ?>" onclick="views_default_templates('template');" class="button" />
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+	<div align="center">
+		<input type="submit" name="do" class="button" value="<?php _e('Update Options', 'wp-postviews'); ?>" />&nbsp;&nbsp;<input type="button" name="cancel" value="<?php _e('Cancel', 'wp-postviews'); ?>" class="button" onclick="javascript:history.go(-1)" /> 
+	</div>
+</div>
+</form> 
+
+<!-- Uninstall WP-PostViews -->
+<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>"> 
+<div class="wrap"> 
+	<h2><?php _e('Uninstall WP-PostViews', 'wp-postviews'); ?></h2>
+	<p style="text-align: left;">
+		<?php _e('Deactivating WP-PostViews plugin does not remove any data that may have been created, such as the views data. To completely remove this plugin, you can uninstall it here.', 'wp-postviews'); ?>
+	</p>
+	<p style="text-align: left; color: red">
+		<strong><?php _e('WARNING:', 'wp-postviews'); ?></strong><br />
+		<?php _e('Once uninstalled, this cannot be undone. You should use a Database Backup plugin of WordPress to back up all the data first.', 'wp-postviews'); ?>
+	</p>
+	<p style="text-align: left; color: red">
+		<strong><?php _e('The following WordPress Options/PostMetas will be DELETED:', 'wp-postviews'); ?></strong><br />
+	</p>
+	<table width="70%"  border="0" cellspacing="3" cellpadding="3">
+		<tr class="thead">
+			<td align="center"><strong><?php _e('WordPress Options', 'wp-postviews'); ?></strong></td>
+			<td align="center"><strong><?php _e('WordPress PostMetas', 'wp-postviews'); ?></strong></td>
+		</tr>
+		<tr>
+			<td valign="top" style="background-color: #eee;">
+				<ol>
+				<?php
+					foreach($views_settings as $settings) {
+						echo '<li>'.$settings.'</li>'."\n";
+					}
+				?>
+				</ol>
+			</td>
+			<td valign="top" style="background-color: #eee;">
+				<ol>
+				<?php
+					foreach($views_postmetas as $postmeta) {
+						echo '<li>'.$postmeta.'</li>'."\n";
+					}
+				?>
+				</ol>
+			</td>
+		</tr>
+	</table>
+	<p>&nbsp;</p>
+	<p style="text-align: center;">
+		<input type="checkbox" name="uninstall_views_yes" value="yes" />&nbsp;<?php _e('Yes', 'wp-postviews'); ?><br /><br />
+		<input type="submit" name="do" value="<?php _e('UNINSTALL WP-PostViews', 'wp-postviews'); ?>" class="button" onclick="return confirm('<?php _e('You Are About To Uninstall WP-PostViews From WordPress.\nThis Action Is Not Reversible.\n\n Choose [Cancel] To Stop, [OK] To Uninstall.', 'wp-postviews'); ?>')" />
+	</p>
+</div> 
+</form>
+<?php
+} // End switch($mode)
+?>
---
+++ postviews/postviews.php
@@ -0,0 +1,388 @@
+<?php
+/*
+Plugin Name: WP-PostViews
+Plugin URI: http://lesterchan.net/portfolio/programming.php
+Description: Enables you to display how many times a post/page had been viewed. It will not count registered member views, but that can be changed easily.
+Version: 1.20
+Author: Lester 'GaMerZ' Chan
+Author URI: http://lesterchan.net
+*/
+
+
+/*  
+	Copyright 2007  Lester Chan  (email : gamerz84@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+### Create Text Domain For Translations
+add_action('init', 'postviews_textdomain');
+function postviews_textdomain() {
+	load_plugin_textdomain('wp-postviews', 'wp-content/plugins/postviews');
+}
+
+
+### Function: Post Views Option Menu
+add_action('admin_menu', 'postviews_menu');
+function postviews_menu() {
+	if (function_exists('add_options_page')) {
+		add_options_page(__('Post Views', 'wp-postviews'), __('Post Views', 'wp-postviews'), 'manage_options', 'postviews/postviews-options.php') ;
+	}
+}
+
+
+### Function: Calculate Post Views
+add_action('loop_start', 'process_postviews');
+function process_postviews() {
+	global $id, $user_ID;
+	$views_options = get_option('views_options');
+	$post_views = get_post_custom($post_id);
+	$post_views = intval($post_views['views'][0]);
+	$should_count = false;
+	switch(intval($views_options['count'])) {
+		case 0:
+			$should_count = true;
+			break;
+		case 1:
+			if(empty($_COOKIE[USER_COOKIE])) {
+				$should_count = true;
+			}
+			break;
+		case 2:
+			if(intval($user_ID) > 0) {
+				$should_count = true;
+			}
+			break;
+	}
+	if($should_count) {
+		if(is_single() || is_page()) {
+			if(!update_post_meta($id, 'views', ($post_views+1))) {
+				add_post_meta($id, 'views', 1, true);
+			}
+			remove_action('loop_start', 'process_postviews');
+		}	
+	}
+}
+
+
+### Function: Display The Post Views
+function the_views($display = true) {
+	$post_views = intval(post_custom('views'));
+	$views_options = get_option('views_options');
+	$output = str_replace('%VIEW_COUNT%', number_format($post_views), $views_options['template']);
+	if($display) {
+		echo $output;
+	} else {
+		return $output;
+	}
+}
+
+
+### Function: Display Most Viewed Page/Post
+if(!function_exists('get_most_viewed')) {
+	function get_most_viewed($mode = '', $limit = 10, $chars = 0, $display = true) {
+		global $wpdb, $post;
+		$where = '';
+		$temp = '';
+		if(!empty($mode) && $mode != 'both') {
+			$where = "post_type = '$mode'";
+		} else {
+			$where = '1=1';
+		}
+		$most_viewed = $wpdb->get_results("SELECT DISTINCT $wpdb->posts.*, (meta_value+0) AS views FROM $wpdb->posts LEFT JOIN $wpdb->postmeta ON $wpdb->postmeta.post_id = $wpdb->posts.ID WHERE post_date < '".current_time('mysql')."' AND $where AND post_status = 'publish' AND meta_key = 'views' AND post_password = '' ORDER  BY views DESC LIMIT $limit");
+		if($most_viewed) {
+			if($chars > 0) {
+				foreach ($most_viewed as $post) {
+					$post_title = get_the_title();
+					$post_views = intval($post->views);
+					$post_views = number_format($post_views);
+					$temp .= "<li><a href=\"".get_permalink()."\">".snippet_chars($post_title, $chars)."</a> - $post_views ".__('views', 'wp-postviews')."</li>\n";
+				}
+			} else {
+				foreach ($most_viewed as $post) {
+					$post_title = get_the_title();
+					$post_views = intval($post->views);
+					$post_views = number_format($post_views);
+					$temp .= "<li><a href=\"".get_permalink()."\">$post_title</a> - $post_views ".__('views', 'wp-postviews')."</li>\n";
+				}
+			}
+		} else {
+			$temp = '<li>'.__('N/A', 'wp-postviews').'</li>'."\n";
+		}
+		if($display) {
+			echo $temp;
+		} else {
+			return $temp;
+		}
+	}
+}
+
+
+### Function: Display Most Viewed Page/Post By Category ID
+if(!function_exists('get_most_viewed_category')) {
+	function get_most_viewed_category($category_id = 0, $mode = '', $limit = 10, $chars = 0, $display = true) {
+		global $wpdb, $post;
+		$where = '';
+		$temp = '';
+		if(is_array($category_id)) {
+			$category_sql = "$wpdb->term_relationships.term_taxonomy_id IN (".join(',', $category_id).')';
+		} else {
+			$category_sql = "$wpdb->term_relationships.term_taxonomy_id = $category_id";
+		}
+		if(!empty($mode) && $mode != 'both') {
+			$where = "post_type = '$mode'";
+		} else {
+			$where = '1=1';
+		}
+		$most_viewed = $wpdb->get_results("SELECT DISTINCT $wpdb->posts.*, (meta_value+0) AS views FROM $wpdb->posts LEFT JOIN $wpdb->postmeta ON $wpdb->postmeta.post_id = $wpdb->posts.ID LEFT JOIN $wpdb->term_relationships ON $wpdb->term_relationships.object_id = $wpdb->posts.ID WHERE post_date < '".current_time('mysql')."' AND $category_sql AND $where AND post_status = 'publish' AND meta_key = 'views' AND post_password = '' ORDER  BY views DESC LIMIT $limit");
+		if($most_viewed) {
+			if($chars > 0) {
+				foreach ($most_viewed as $post) {
+					$post_title = htmlspecialchars(stripslashes($post->post_title));
+					$post_views = intval($post->views);
+					$post_views = number_format($post_views);
+					$temp .= "<li><a href=\"".get_permalink()."\">".snippet_chars($post_title, $chars)."</a> - $post_views ".__('views', 'wp-postviews')."</li>\n";
+				}
+			} else {
+				foreach ($most_viewed as $post) {
+					$post_title = htmlspecialchars(stripslashes($post->post_title));
+					$post_views = intval($post->views);
+					$post_views = number_format($post_views);
+					$temp .= "<li><a href=\"".get_permalink()."\">$post_title</a> - $post_views ".__('views', 'wp-postviews')."</li>\n";
+				}
+			}
+		} else {
+			$temp = '<li>'.__('N/A', 'wp-postviews').'</li>'."\n";
+		}
+		if($display) {
+			echo $temp;
+		} else {
+			return $temp;
+		}
+	}
+}
+
+
+### Function: Get TimeSpan Most Viewed - Added by Paolo Tagliaferri (http://www.vortexmind.net - webmaster@vortexmind.net)
+function get_timespan_most_viewed($mode = '', $limit = 10, $days = 7, $display = true) {
+	global $wpdb, $post;	
+	$limit_date = current_time('timestamp') - ($days*86400); 
+	$limit_date = date("Y-m-d H:i:s",$limit_date);	
+	$where = '';
+	$temp = '';
+	if(!empty($mode) && $mode != 'both') {
+		$where = "post_type = '$mode'";
+	} else {
+		$where = '1=1';
+	}
+	$most_viewed = $wpdb->get_results("SELECT DISTINCT $wpdb->posts.*, (meta_value+0) AS views FROM $wpdb->posts LEFT JOIN $wpdb->postmeta ON $wpdb->postmeta.post_id = $wpdb->posts.ID WHERE post_date < '".current_time('mysql')."' AND post_date > '".$limit_date."' AND $where AND post_status = 'publish' AND meta_key = 'views' AND post_password = '' ORDER  BY views DESC LIMIT $limit");
+	if($most_viewed) {
+		foreach ($most_viewed as $post) {
+			$post_title = get_the_title();
+			$post_views = intval($post->views);
+			$post_views = number_format($post_views);
+			$temp .= "<li><a href=\"".get_permalink()."\">$post_title</a> - $post_views ".__('views', 'wp-postviews')."</li>";
+		}
+	} else {
+		$temp = '<li>'.__('N/A', 'wp-postviews').'</li>'."\n";
+	}
+	if($display) {
+		echo $temp;
+	} else {
+		return $temp;
+	}
+}
+
+
+### Function: Get TimeSpan Most Viewed By Category
+function get_timespan_most_viewed_cat($category_id = 0, $mode = '', $limit = 10, $days = 7, $display = true) {
+	global $wpdb, $post;	
+	$limit_date = current_time('timestamp') - ($days*86400); 
+	$limit_date = date("Y-m-d H:i:s",$limit_date);	
+	$where = '';
+	$temp = '';
+	if(is_array($category_id)) {
+		$category_sql = "$wpdb->term_relationships.term_taxonomy_id IN (".join(',', $category_id).')';
+	} else {
+		$category_sql = "$wpdb->term_relationships.term_taxonomy_id = $category_id";
+	}
+	if(!empty($mode) && $mode != 'both') {
+		$where = "post_type = '$mode'";
+	} else {
+		$where = '1=1';
+	}
+	$most_viewed = $wpdb->get_results("SELECT DISTINCT $wpdb->posts.*, (meta_value+0) AS views FROM $wpdb->posts LEFT JOIN $wpdb->postmeta ON $wpdb->postmeta.post_id = $wpdb->posts.ID LEFT JOIN $wpdb->term_relationships ON $wpdb->term_relationships.object_id = $wpdb->posts.ID WHERE post_date < '".current_time('mysql')."' AND $category_sql AND post_date > '".$limit_date."' AND $where AND post_status = 'publish' AND meta_key = 'views' AND post_password = '' ORDER  BY views DESC LIMIT $limit");
+	if($most_viewed) {
+		foreach ($most_viewed as $post) {
+			$post_title = get_the_title();
+			$post_views = intval($post->views);
+			$post_views = number_format($post_views);
+			$temp .= "<li><a href=\"".get_permalink()."\">$post_title</a> - $post_views ".__('views', 'wp-postviews')."</li>";
+		}
+	} else {
+		$temp = '<li>'.__('N/A', 'wp-postviews').'</li>'."\n";
+	}
+	if($display) {
+		echo $temp;
+	} else {
+		return $temp;
+	}
+}
+
+
+### Function: Display Total Views
+if(!function_exists('get_totalviews')) {
+	function get_totalviews($display = true) {
+		global $wpdb;
+		$total_views = $wpdb->get_var("SELECT SUM(meta_value+0) FROM $wpdb->postmeta WHERE meta_key = 'views'");
+		if($display) {
+			echo number_format($total_views);
+		} else {
+			return number_format($total_views);
+		}
+	}
+}
+
+
+### Function: Snippet Text
+if(!function_exists('snippet_chars')) {
+	function snippet_chars($text, $length = 0) {
+		$text = htmlspecialchars_decode($text);
+		 if (strlen($text) > $length){       
+			return htmlspecialchars(substr($text,0,$length)).'...';             
+		 } else {
+			return htmlspecialchars($text);
+		 }
+	}
+}
+
+
+### Function: HTML Special Chars Decode
+if (!function_exists('htmlspecialchars_decode')) {
+   function htmlspecialchars_decode($text) {
+       return strtr($text, array_flip(get_html_translation_table(HTML_SPECIALCHARS)));
+   }
+}
+
+
+### Function: Modify Default WordPress Listing To Make It Sorted By Post Views
+function views_fields($content) {
+	global $wpdb;
+	$content .= ", ($wpdb->postmeta.meta_value+0) AS views";
+	return $content;
+}
+function views_join($content) {
+	global $wpdb;
+	$content .= " LEFT JOIN $wpdb->postmeta ON $wpdb->postmeta.post_id = $wpdb->posts.ID";
+	return $content;
+}
+function views_where($content) {
+	global $wpdb;
+	$content .= " AND $wpdb->postmeta.meta_key = 'views'";
+	return $content;
+}
+function views_orderby($content) {
+	$orderby = trim(addslashes($_GET['orderby']));
+	if(empty($orderby) || ($orderby != 'asc' && $orderby != 'desc')) {
+		$orderby = 'desc';
+	}
+	$content = " views $orderby";
+	return $content;
+}
+
+
+### Process The Sorting
+/*
+if($_GET['sortby'] == 'views') {
+	add_filter('posts_fields', 'views_fields');
+	add_filter('posts_join', 'views_join');
+	add_filter('posts_where', 'views_where');
+	add_filter('posts_orderby', 'views_orderby');
+}
+*/
+
+
+### Function: Plug Into WP-Stats
+if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php') || strpos($_SERVER['REQUEST_URI'], 'stats/stats.php')) {
+	add_filter('wp_stats_page_admin_plugins', 'postviews_page_admin_general_stats');
+	add_filter('wp_stats_page_admin_most', 'postviews_page_admin_most_stats');
+	add_filter('wp_stats_page_plugins', 'postviews_page_general_stats');
+	add_filter('wp_stats_page_most', 'postviews_page_most_stats');
+}
+
+
+### Function: Add WP-PostViews General Stats To WP-Stats Page Options
+function postviews_page_admin_general_stats($content) {
+	$stats_display = get_option('stats_display');
+	if($stats_display['views'] == 1) {
+		$content .= '<input type="checkbox" name="stats_display[]" id="wpstats_views" value="views" checked="checked" />&nbsp;&nbsp;<label for="wpstats_views">'.__('WP-PostViews', 'wp-postviews').'</label><br />'."\n";
+	} else {
+		$content .= '<input type="checkbox" name="stats_display[]" id="wpstats_views" value="views" />&nbsp;&nbsp;<label for="wpstats_views">'.__('WP-PostViews', 'wp-postviews').'</label><br />'."\n";
+	}
+	return $content;
+}
+
+
+### Function: Add WP-PostViews Top Most/Highest Stats To WP-Stats Page Options
+function postviews_page_admin_most_stats($content) {
+	$stats_display = get_option('stats_display');
+	$stats_mostlimit = intval(get_option('stats_mostlimit'));
+	if($stats_display['viewed_most'] == 1) {
+		$content .= '<input type="checkbox" name="stats_display[]" id="wpstats_viewed_most" value="viewed_most" checked="checked" />&nbsp;&nbsp;<label for="wpstats_viewed_most">'.$stats_mostlimit.' '.__('Most Viewed Posts', 'wp-postviews').'</label><br />'."\n";
+	} else {
+		$content .= '<input type="checkbox" name="stats_display[]" id="wpstats_viewed_most" value="viewed_most" />&nbsp;&nbsp;<label for="wpstats_viewed_most">'.$stats_mostlimit.' '.__('Most Viewed Posts', 'wp-postviews').'</label><br />'."\n";
+	}
+	return $content;
+}
+
+
+### Function: Add WP-PostViews General Stats To WP-Stats Page
+function postviews_page_general_stats($content) {
+	$stats_display = get_option('stats_display');
+	if($stats_display['views'] == 1) {
+		$content .= '<p><strong>'.__('WP-PostViews', 'wp-postviews').'</strong></p>'."\n";
+		$content .= '<ul>'."\n";
+		$content .= '<li><strong>'.get_totalviews(false).'</strong> '.__('views were generated.', 'wp-postviews').'</li>'."\n";
+		$content .= '</ul>'."\n";
+	}
+	return $content;
+}
+
+
+### Function: Add WP-PostViews Top Most/Highest Stats To WP-Stats Page
+function postviews_page_most_stats($content) {
+	$stats_display = get_option('stats_display');
+	$stats_mostlimit = intval(get_option('stats_mostlimit'));
+	if($stats_display['viewed_most'] == 1) {
+		$content .= '<p><strong>'.$stats_mostlimit.' '.__('Most Viewed Post', 'wp-postviews').'</strong></p>'."\n";
+		$content .= '<ul>'."\n";
+		$content .= get_most_viewed('post', $stats_mostlimit, 0, false);
+		$content .= '</ul>'."\n";
+	}
+	return $content;
+}
+
+
+### Function: Post Views Options
+add_action('activate_postviews/postviews.php', 'views_init');
+function views_init() {
+	// Add Options
+	$views_options = array();
+	$views_options['count'] = 1;
+	$views_options['template'] = __('%VIEW_COUNT% views', 'wp-postviews');
+	add_option('views_options', $views_options, 'Post Views Options');
+}
+?>
