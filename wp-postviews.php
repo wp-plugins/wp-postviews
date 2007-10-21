567e4766ca5396d971cf3aa4f6f544f191838fd1
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-21 14:58:32 +0000

    Change WP-Stats Path
    git-svn-id: https://plugins.svn.wordpress.org/wp-postviews/trunk@22754 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-postviews.php
+++ wp-postviews.php
@@ -316,7 +316,7 @@ if($_GET['sortby'] == 'views') {
 
 
 ### Function: Plug Into WP-Stats
-if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php') || strpos($_SERVER['REQUEST_URI'], 'stats/stats.php')) {
+if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php') || strpos($_SERVER['REQUEST_URI'], 'wp-stats/wp-stats.php')) {
 	add_filter('wp_stats_page_admin_plugins', 'postviews_page_admin_general_stats');
 	add_filter('wp_stats_page_admin_most', 'postviews_page_admin_most_stats');
 	add_filter('wp_stats_page_plugins', 'postviews_page_general_stats');
