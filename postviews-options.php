248b4c2f2cd2edb0c27bc9ba6a4e56cc9136ea64
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-18 01:23:49 +0000

    1.60 Release
    git-svn-id: https://plugins.svn.wordpress.org/wp-postviews/trunk@347158 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postviews-options.php
+++ postviews-options.php
@@ -256,7 +256,7 @@ switch($mode) {
 <p>&nbsp;</p>
 
 <!-- Uninstall WP-PostViews -->
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=<?php echo plugin_basename(__FILE__); ?>">
+<form method="post" action="<?php echo admin_url('admin.php?page='.plugin_basename(__FILE__)); ?>">
 <div class="wrap"> 
 	<h3><?php _e('Uninstall WP-PostViews', 'wp-postviews'); ?></h3>
 	<p>
